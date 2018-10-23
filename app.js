7b78e10ba017797372faa7d9c3bab5bc96b22239
niesssiobhan <niesssiobhan@live.com>
2018-10-23 13:19:05 -0700

    in class work

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 'use strict';
 
 //array to store images
+var name = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 var mallItems = [];
+var totalClicks = [];
 
 //make a constructor function
 function Item(name, filepath) {
     this.name = name;
-    this.filepath = filepath;
+    this.filepath = `img/${name}.jpg`
     this.views = 0;
+    this.votes = 0;
     mallItems.push(this)
 }
 
+
 // make new Item instances
-new Item('starwars bag', 'img/bag (1).jpg');
+
+// for (var i = 0; i < names.length; i++){
+//     new Item(names[i]);
+// }
+
+new Item('bag', 'img/bag.jpg');
 new Item('banana', 'img/banana.jpg');
 new Item('bathroom', 'img/bathroom.jpg');
 new Item('boots', 'img/boots.jpg');
@@ -34,11 +43,12 @@ new Item('water can', 'img/water-can.jpg');
 new Item('wine glass', 'img/wine-glass.jpg');
 
 //get the image element from the DOM
-var mallImgContianer = document.getElementById('image-container');
+var mallImgContainer = document.getElementById('image-container');
 
 var mallImg1 = document.getElementById('item-image-1');
 var mallImg2 = document.getElementById('item-image-2');
 var mallImg3 = document.getElementById('item-image-3');
+
 
 //why is this commented out but still working?
 // a function to randomly display one of the images
@@ -63,37 +73,93 @@ var mallImg3 = document.getElementById('item-image-3');
 ///////////////////////////////////////////////////////////////////////
 mallImgContainer.addEventListener('click', randomImg);
 
-// counter = 0;
-
-//
-function randomInd (){
-    return Math.floor(Math.random() * mallItems.length);
+function randomInt (){
+    return Math.floor(Math.random() * mallItems.length); //this makes a random number
 }
-function randomImg (){
-    var randArray = [];
-    randArray[0] = randomInd();
-    var second = randomInd();
-while(second === randArray[0]){
-    second = randomInd();
+
+function makeThreeUnique(){
+    console.log(justViewed, 'just viewed inline 50');
+    var output = [0];
+
+    var firstNum = randomInt(); //this makes the first
+    while(justViewed.includes(firstNum)) { //if yes then re roll the first number
+        firstNum = randomInt(); //this makes the first again
     }
-    randArray[1] = second;
-    var third = randomInd();
-while(third === randArray[0] || third === randArray[1]){
-    third = randomInd();
+    output.push(firstNum);
+
+    output.push(randomInt()); //this makes the second
+    while (output[0] === output[1]){
+        console.log('duplicate detected');
+        output[1] = randomInt();
+}
+output.push(randomInt()); //this makes the third
+while (output[0] === output[2] || output[1] === output[2]){
+    console.log('duplicate detected');
+    output[2] = randomInt();
+}
+justViewed = output;
+return output ;
+}
+
+function displayImg() {
+    var img = makeThreeUnique();
+    console.log(img, 'three new values in line 69 where we created them after the click');
+    mallItems[img[0]].views++;
+    mallItems[img[1]].views++;
+    mallItems[img[2]].views++;
+    mallImg1.src = mallItems[img[0]].filepath;
+    mallImg2.src = mallItems[img[1]].filepath;
+    mallImg3.src = mallItems[img[2]].filepath;
+    mallImg1.alt = mallItems[img[0]].name;
+    mallImg2.alt = mallItems[img[1]].name;
+    mallImg3.alt = mallItems[img[2]].name;
+    mallImg1.title = mallItems[img[0]].name;
+    mallImg2.title = mallItems[img[1]].name;
+    mallImg3.title = mallItems[img[2]].name;
+}
+
+function handleClicks(event) {
+    console.log(event.target.alt, 'was clicked');
+    if(event.target.id === 'image_containter') {
+        return alert ('Please be sure to click on an image');
     }
-    randArray[2] = third;
+    totalClicks++;
+    console.log(totalClicks, 'total clicks');
+    for (var i = 0; i < mallItems.length; i++) {
+        if(event.target.alt === mallItems[i].name) {
+            mallItems[i].votes++;
+        }
+    }
+    if(totalClicks === 25) {
+        containter.removeEventListener('click', handClick);
+        return showList();
+    }
+    displayImg();
+}
+
+function showList() {
+    for(var i = 0; i < mallItems.length; i++) {
+        var liEl = document.createElement('li');
+        liEl.textContent = `${mallItems[i].name} had ${mallItems[i].views} views and ${mallItems[i].votes} votes`
+        productList.appendChild(iEl);
+}
+
+// function randomImg (){
+//     var randArray = [];
+//     randArray[0] = randomInt();
+//     var second = randomInt();
+// while(second === randArray[0]){
+//     second = randomInt();
+//     }
+//     randArray[1] = second;
+//     var third = randomInt();
+// while(third === randArray[0] || third === randArray[1]){
+//     third = randomInt();
+//     }
+//     randArray[2] = third;
     
-    console.log(randArray)
+//     console.log(randArray)
    
-    mallImg1.src = mallItems[randArray[0]].filepath;
-    mallImg1.alt = mallItems[randArray[0]].name;
-    mallImg1.title = mallItems[randArray[0]].name;
-    mallImg2.src = mallItems[randArray[1]].filepath;
-    mallImg2.alt = mallItems[randArray[1]].name;
-    mallImg2.title = mallItems[randArray[1]].name;
-    mallImg3.src = mallItems[randArray[2]].filepath;
-    mallImg3.alt = mallItems[randArray[2]].name;
-    mallImg3.title = mallItems[randArray[2]].name;
-}
 
-randomImg()
+displayImg();
+container.addEventListener('click', handleClick);
